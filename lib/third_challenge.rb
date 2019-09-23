296c8f38ec76a70e4a4e675add29c54fc6b93975
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-23 14:54:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,20 +1,20 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name => "Lord Montague", :age => "53"
+      :patriarch => {name: => "Lord Montague", age: => "53"
         },
-      :matriarch => {:name => "lady Montague", :age =>"54"
+      :matriarch => {name: => "lady Montague", age: =>"54"
         },
-      :hero => {:name => "Romeo", :age => "15", :status => "alive"
+      :hero => {name: => "Romeo", age: => "15", status: => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {:name => "Lord Capulet", :age => "50"
+      :patriarch => {name: => "Lord Capulet", age: => "50"
         },
-      :matriarch => {:name => "Lady Capulet", :age => "51"
+      :matriarch => {name: => "Lady Capulet", age: => "51"
         },
-      :heroine => {:name => "Juliet", :age => "15", :status => "alive"
+      :heroine => {name: => "Juliet", age: => "15", status: => "alive"
         },
       :heroine_friends => []
    }
